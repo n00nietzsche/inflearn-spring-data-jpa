c26c93107485808b8c4fc9ddbd86053e5998c07d
Jake Seo <n00nietzsche@gmail.com>
2021-01-11 22:20:03 +0900

    first commit

---
+++ src/main/java/study/datajpa/JakeSeoApplication.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakeSeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakeSeoApplication.class, args);
+	}
+
+}
